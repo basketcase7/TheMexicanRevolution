99093288f9ce2166a5626d524d632cbb71399dc7
basketcase7 <crazser@gmail.com>
2024-07-08 22:07:03 +0700

    111

--- scroll.js
+++ scroll.js
@@ -6,3 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
     card.scrollIntoView({ behavior: "smooth" });
   });
 });
+
+function redirectToIndex() {
+  window.location.href = "index.html";
+}
+function redirectToEvents() {
+  window.location.href = "events.html";
+}
+function redirectToChars() {
+  window.location.href = "characters.html";
+}
+function redirectToValue() {
+  window.location.href = "value.html";
+}
+function redirectToIlls() {
+  window.location.href = "illustrations.html";
+}
+function redirectToTest() {
+  window.location.href = "test.html";
+}
+function redirectToContacts() {
+  window.location.href = "contacts.html";
+}
